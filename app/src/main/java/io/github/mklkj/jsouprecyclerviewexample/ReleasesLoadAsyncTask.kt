c2ee651561578146d1d58bb00d3d166873d95e69
Mikołaj Pich <m.pich@outlook.com>
2020-03-28 17:02:35 +0100

    Handle errors while loading

--- app/src/main/java/io/github/mklkj/jsouprecyclerviewexample/ReleasesLoadAsyncTask.kt
+++ app/src/main/java/io/github/mklkj/jsouprecyclerviewexample/ReleasesLoadAsyncTask.kt
@@ -1,8 +1,12 @@
 package io.github.mklkj.jsouprecyclerviewexample
 
 import android.os.AsyncTask
+import android.util.Log
 import android.view.View
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
 import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import org.jsoup.Jsoup
 import java.lang.ref.WeakReference
@@ -14,28 +18,44 @@ class ReleasesLoadAsyncTask(activity: MainActivity, adapter: ReleasesAdapter) :
 
     private val adapter: WeakReference<ReleasesAdapter> = WeakReference(adapter)
 
+    private var lastError: Throwable? = null
+
     override fun onPreExecute() {
         activity.get()?.run {
-            findViewById<RecyclerView>(R.id.recycler_releases)?.visibility = View.INVISIBLE
-            findViewById<ProgressBar>(R.id.progress_releases)?.visibility = View.VISIBLE
+            findViewById<RecyclerView>(R.id.recycler_releases)?.visibility = INVISIBLE
+            findViewById<TextView>(R.id.message_releases)?.visibility = INVISIBLE
+            findViewById<ProgressBar>(R.id.progress_releases)?.visibility = VISIBLE
         }
     }
 
     override fun doInBackground(vararg params: String?): List<ReleaseItem> {
-        return Jsoup.connect(params[0]).get().select(".toc ol a").map {
-            ReleaseItem(it.text(), it.attr("abs:href"))
+        return try {
+            Jsoup.connect(params[0]).get().select(".toc ol a").map {
+                ReleaseItem(it.text(), it.attr("abs:href"))
+            }.also {
+                lastError = null
+            }
+        } catch (e: Throwable) {
+            Log.e("ReleasesLoadAsyncTask", "Error: ${e.message}", e)
+            lastError = e
+            emptyList()
         }
     }
 
     override fun onPostExecute(result: List<ReleaseItem>) {
-        adapter.get()?.run {
-            items = result
-            notifyDataSetChanged()
+        if (result.isNotEmpty()) {
+            adapter.get()?.run {
+                items = result
+                notifyDataSetChanged()
+            }
+            activity.get()?.findViewById<RecyclerView>(R.id.recycler_releases)?.visibility = VISIBLE
+        } else {
+            activity.get()?.findViewById<TextView>(R.id.message_releases)?.run {
+                visibility = VISIBLE
+                text = if (lastError == null) "No items found" else lastError?.message
+            }
         }
 
-        activity.get()?.run {
-            findViewById<RecyclerView>(R.id.recycler_releases)?.visibility = View.VISIBLE
-            findViewById<ProgressBar>(R.id.progress_releases)?.visibility = View.GONE
-        }
+        activity.get()?.findViewById<ProgressBar>(R.id.progress_releases)?.visibility = View.GONE
     }
 }
